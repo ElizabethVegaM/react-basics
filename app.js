62533d3b998e0df493cae28cc71ca4a40466ba53
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-09-06 12:28:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+/*
+document.getElementById('app').innerHTML = '<p id="title">Arriba React, abajo los aburridos de angular</p>';/
+
+
+const parrafo = document.createElement('p');
+const spanTexto = document.createElement('span');
+parrafo.setAttribute('id', 'title');
+parrafo.textContent = 'Angular';
+spanTexto.textContent = ' es lo mejor';
+
+parrafo.appendChild(spanTexto);
+document.getElementById('app').appendChild(parrafo);
+
+
+// REACT
+// Tipo de elemento , atributo , hijos
+const elemento = React.createElement('p', { id: 'title' }, 'Angular ', React.createElement('span', null, 'es lo mejor' ));// null indica que el parametro no existe
+
+
+// Metodo render( elemento , donde va )
+ReactDOM.render(elemento, document.getElementById('app'))
+
+
+// JSX
+// definir las class de html como className, dado que class está reservada para componentes en React
+const elemento = 
+(<p id="title" className="title">React {'javascript'.toUpperCase()} 
+  <span> es lo mejor</span>
+</p>)
+ReactDOM.render(elemento, document.getElementById('app'));
+*/
+
+// los componentes deben comenzar con mayúsculas
+// un parámetro puede ir suelto
+// const Saludar = props => <p>Hola {props.nombre}</p> 
+// dos propiedades o más, se utilizan paréntesis y llaves
+const Saludar = ({nombre, apellido}) => {
+  return (
+    <div>
+      <p>Hola {nombre}</p>
+      <p>Apellido: {apellido}</p>
+    </div>)
+};
+
+ReactDOM.render(<Saludar nombre='Ely' apellido="Vega"/>, document.getElementById('app'))
